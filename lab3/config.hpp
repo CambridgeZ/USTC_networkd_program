7a731460bf44f6c21d012fc94bbc0b45505acfb0
CambridgeZ <1376734195@qq.com>
2024-12-12 17:41:33 +0800

    🥳finish this course

--- lab3/config.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef __CONFIG_HPP
-#define __CONFIG_HPP
-
-#define IP "127.0.0.1"
-#define PORT 12345
-
-#endif // __CONFIG_HPP
--- lab3/file_send_GUI/build/CMakeFiles/FileTransferApp.dir/FileTransferApp_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/FileTransferApp.dir/FileTransferApp_autogen/mocs_compilation.cpp.o: \
- /home/ubuntu/USTC_networkd_program/lab3/file_send_GUI/build/FileTransferApp_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
